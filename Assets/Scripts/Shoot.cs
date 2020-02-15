2f35dc50591049aca3fad02fd4b578adc47b2ae3
Florian Landry <florian.landry71@gmail.com>
2020-02-15 02:24:41 -0500

    Fini bis !

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -12,36 +12,43 @@ public class Shoot : MonoBehaviour
     UnityEngine.UI.Image image;
     int tempsMort = 0;
     int tempsAttente = 8;
-    int tempsMortSkill = 180;
-    int coolDownSkill = 0;
+    float tempsMortSkill = 1.8f;
+    float coolDownSkill = 0f;
+    private bool canCast = false;
 
     void Update()
     {
-        image.color = new Color(image.color.r, image.color.g, image.color.b, (tempsMortSkill*100)/180);
+        image.color = new Color(image.color.r, image.color.g, image.color.b, tempsMortSkill*100/180);
+        if(tempsMortSkill <= coolDownSkill)
+        {
+            canCast = true;
+        }
         if (Input.GetMouseButton(1))
         {
             if (Input.GetMouseButton(0) && tempsMort >= tempsAttente)
             {
                 shoot();
-                tempsMort = 0;
             }
             tempsMort++;
-            if (Input.GetMouseButtonDown(2) && tempsMortSkill <= coolDownSkill)
+            if (Input.GetMouseButtonDown(2) && canCast)
             {
                 skill();
-                tempsMortSkill = 180;
             }
-            tempsMortSkill--;
+            
         }
+        tempsMortSkill -= 0.01f;
     }
 
     public void shoot()
     {
         cannonParticleShooter.Play();
+        tempsMort = 0;
     }
 
     public void skill()
     {
         megaAttack.Play();
+        canCast = false;
+        tempsMortSkill = 1.8f;
     }
 }
