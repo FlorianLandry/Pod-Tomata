6906bc69aaf6d91fc879ff2f2ef637a9528b96d4
Florian Landry <florian.landry71@gmail.com>
2020-02-15 02:01:28 -0500

    Fini !

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -6,8 +6,12 @@ public class Shoot : MonoBehaviour
 {
     [SerializeField]
     ParticleSystem cannonParticleShooter;
+    [SerializeField]
+    ParticleSystem megaAttack;
     int tempsMort = 0;
     int tempsAttente = 8;
+    int tempsMortSkill = 0;
+    int coolDownSkill = 180;
 
     void Update()
     {
@@ -19,11 +23,22 @@ public class Shoot : MonoBehaviour
                 tempsMort = 0;
             }
             tempsMort++;
+            if (Input.GetMouseButtonDown(2) && tempsMortSkill >= coolDownSkill)
+            {
+                skill();
+                tempsMortSkill = 0;
+            }
+            tempsMortSkill++;
         }
     }
 
     public void shoot()
     {
         cannonParticleShooter.Play();
+    }
+
+    public void skill()
+    {
+        megaAttack.Play();
     }
 }
