1a6f5d8fcf689c8b9ed5bb8727a94e74645d000e
Florian Landry <florian.landry71@gmail.com>
2020-02-14 22:58:09 -0500

    Ajout du projet sur git

---
+++ Assets/Scripts/DetectCube.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class DetectCube : MonoBehaviour
+{
+    [SerializeField]
+    private BoxCollider colliderPod;
+    private List<GameObject> objetsDansCollider;
+    private Transform target;
+    [SerializeField]
+    private TurnAround enRotation;
+
+    private void Start()
+    {
+        objetsDansCollider = new List<GameObject>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "ennemi")
+        {
+            objetsDansCollider.Add(other.gameObject);
+            Debug.Log("Ajout d'un ennemi !");
+        }
+        
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "ennemi")
+        {
+            objetsDansCollider.Remove(other.gameObject);
+            transform.LookAt(new Vector3(transform.position.x + 5, transform.position.y, transform.position.z));
+            Debug.Log("Suppression d'un ennemi !");
+        }
+    }
+
+    private void Update()
+    {
+        if (objetsDansCollider.Any())
+        {
+            float distanceMin = -1;
+            foreach (GameObject game in objetsDansCollider)
+            {
+                if(distanceMin < 0 || distanceMin > Vector3.Distance(game.transform.position, transform.position))
+                {
+                    distanceMin = Vector3.Distance(game.transform.position, transform.position);
+                    target = game.transform;
+                }
+            }
+        }
+        else
+        {
+            target = null;
+        }
+
+        if(target != null)
+        {
+            transform.LookAt(target);
+            enRotation.enabled = false;
+        }
+        else
+        {
+            enRotation.enabled = true;
+        }
+    }
+}
---
+++ Assets/Scripts/TurnAround.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TurnAround : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target;
+
+    void Update()
+    {
+        transform.RotateAround(target.position, Vector3.up, -30 * Time.deltaTime);
+    }
+}
